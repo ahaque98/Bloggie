7a1431bb619b509e6e763a211462d563c9a74d62
ahaque98 <dev.ahsan98@gmail.com>
2024-08-14 11:25:24 +0600

    Added Controller and html file for AddTags

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly ILogger<AdminTagsController> _logger;
+        public AdminTagsController(ILogger<AdminTagsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
