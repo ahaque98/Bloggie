e8abcedb68e371c4222ae0dafcaf1d2c341814b1
ahaque98 <dev.ahsan98@gmail.com>
2024-08-14 11:08:05 +0600

    UI Improvements on Navbar

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -8,7 +8,7 @@ namespace Bloggie.Web.Data
         public BloggieDbContext(DbContextOptions<BloggieDbContext> options): base(options)
         {
             
-        }
+        } 
 
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
