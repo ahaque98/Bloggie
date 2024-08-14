bdaf50f1fb50ae4be0fbe21e001931c6b3060e38
ahaque98 <dev.ahsan98@gmail.com>
2024-08-14 22:57:47 +0600

    Added Adding functionalities for  Tags

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,19 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
         private readonly ILogger<AdminTagsController> _logger;
-        public AdminTagsController(ILogger<AdminTagsController> logger)
+        private readonly BloggieDbContext _context;
+        public AdminTagsController(ILogger<AdminTagsController> logger, BloggieDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                // Mapping the AddTagRequest viewModel to Tag model
+                var tag = new Tag
+                {
+                    Name = addTagRequest.Name,
+                    DisplayName = addTagRequest.DisplayName
+                };
+
+                _context.Tags.Add(tag);
+
+                _logger.LogInformation($"----------Tag {tag.Name} added successfully to Db----------");
+
+                _context.SaveChanges();
+
+                _logger.LogInformation("----------DB savechanges successfull----------");
+            }
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            IEnumerable<Tag> tags = _context.Tags.ToList();
+
+            return View(tags);
         }
     }
 }
---
+++ Bloggie.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggie.Web.Models.ViewModels
+#nullable disable
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
