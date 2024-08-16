d772bc18152f6253797a8639dbbfdd4996e91f63
ahaque98 <dev.ahsan98@gmail.com>
2024-08-16 14:18:25 +0600

    Added Select Options

---
+++ Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,35 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Bloggie.Web.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagInterface tagRepository;
+        public AdminBlogPostsController(ITagInterface tagRepository)
+        {
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            // Get all tags from repository
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(t => new SelectListItem
+                {
+                    Value = t.Id.ToString(),
+                    Text = t.Name
+                })
+            };
+
+            return View(model);
+        }
+    }
+}
