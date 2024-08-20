b0dbfb53f40d3f39ff3e3986a35d9bbec418fb25
ahaque98 <dev.ahsan98@gmail.com>
2024-08-20 14:14:18 +0600

    Added IdentityDbContext

--- Bloggie.Web/Data/BloggieDbContext.cs
+++ Bloggie.Web/Data/BloggieDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Bloggie.Web.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Data
 { 
-    public class BloggieDbContext : DbContext
+    public class BloggieDbContext : IdentityDbContext
     {
         public BloggieDbContext(DbContextOptions<BloggieDbContext> options): base(options)
         {
@@ -12,5 +14,83 @@ namespace Bloggie.Web.Data
 
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var adminRoleId = "ed2e65a-ce53-45f2-ad10-b7fce202109d";
+            var superAdminRoleId = "d3b07384-d9a7-4b2b-8a1d-0c9e5b1a5e1f";
+            var userRoleId = "94726a44-4a07-4743-9e9b-e597843fe607";
+
+            // Seed roles (User, Admin, SuperAdmin)
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "Admin",
+                    Id=adminRoleId,
+                    ConcurrencyStamp = adminRoleId,
+                },
+                new IdentityRole
+                {
+                    Name="SuperAdmin",
+                    NormalizedName="SuperAdmin",
+                    Id=superAdminRoleId,
+                    ConcurrencyStamp = superAdminRoleId,
+                },
+                new IdentityRole
+                {
+                    Name="User",
+                    NormalizedName="User",
+                    Id=userRoleId,
+                    ConcurrencyStamp = userRoleId,
+                }
+            };
+
+            // Insert roles into database
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // Seed SuperAdmin user
+            var superAdminId = "1f7b8bac-21ec-4dab-8554-8722caf69cc4";
+
+            var superAdminUser = new IdentityUser
+            {
+                UserName = "superadmin@bloggie.com",
+                Email = "superadmin@bloggie.com",
+                NormalizedEmail = "superadmin@bloggie.com".ToUpper(),
+                NormalizedUserName = "superadmin@bloggie.com".ToUpper(),
+                Id = superAdminId
+            };
+
+            // Hashing password
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>()
+                .HashPassword(superAdminUser, "Superadmin@123");
+
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            // Add all roles to SuperAdmin User
+            var superAdminRoles = new List<IdentityUserRole<string>>
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = superAdminRoleId,
+                    UserId = superAdminId
+                },
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
     }
 }
